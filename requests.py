8888f53c54cda98343d5554d913fd202a855e301
Tahseen <75565916+iTahseen@users.noreply.github.com>
2025-11-04 21:54:06 +0500

    Update requests.py

--- requests.py
+++ requests.py
@@ -84,7 +84,7 @@ def format_user(user):
     )
 
 async def fetch_users(session, token):
-    url = "https://api.meeff.com/user/explore/v2/?lat=33.589510&lng=-117.860909"
+    url = "https://api.meeff.com/user/explore/v2/?lat=29.6163714&lng=71.984266"
     headers = {"meeff-access-token": token, "Connection": "keep-alive"}
     async with session.get(url, headers=headers) as response:
         return (await response.json()).get("users", [])
